c97ad1baf30a140c4439a037281b9a2d7459638e
yangeunch <125236669+yangeunch@users.noreply.github.com>
2023-04-21 18:07:42 +0900

    Add files via upload

---
+++ 0421/header.php
@@ -0,0 +1,8 @@
+<header>
+        <h1><a href="./index.html"><img src="#" alt="네이버"></a></h1>
+        <nav>
+            <a href="./mail.html">메일</a>
+            <a href="./cafe.html">카페</a>
+            <a href="#">블로그</a>
+        </nav>
+    </header>
